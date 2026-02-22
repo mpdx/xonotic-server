0c490ce45eae28724c401ccff4793e50e86dd5a4
mpdx <mpdx@users.noreply.github.com>
2026-02-22 15:39:48 +0100

    chore: temporary

--- xonotic/entrypoint.sh
+++ xonotic/entrypoint.sh
@@ -7,20 +7,6 @@ fi
 
 envsubst < /opt/xonotic/server.cfg.template > /opt/xonotic/data/server.cfg.tmp
 
-CUSTOM_MAPS=""
-if [ -d "/custom-maps" ] && [ "$(ls -A /custom-maps/*.pk3 2>/dev/null)" ]; then
-    for map_file in /custom-maps/*.pk3; do
-        if [ -f "$map_file" ]; then
-            map_name=$(basename "$map_file" .pk3)
-            CUSTOM_MAPS="$CUSTOM_MAPS $map_name"
-        fi
-    done
-fi
-
-if [ -n "$CUSTOM_MAPS" ]; then
-    sed -i "/^g_maplist/s/\"$/ $CUSTOM_MAPS\"/" /opt/xonotic/data/server.cfg.tmp
-fi
-
 mv /opt/xonotic/data/server.cfg.tmp /opt/xonotic/data/server.cfg
 
 echo "=== Xonotic Server Configuration ==="
